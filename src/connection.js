da05187ab779c713585b0e8ab24e5ae19e735af9
Will Papp <wpapp90@gmail.com>
2021-03-23 16:00:55 -0500

    updated variable password for connection

--- src/connection.js
+++ src/connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'Qazwsx1213765',
+    password: process.env.PASSWORD,
     database: 'database_db'
 });
 
